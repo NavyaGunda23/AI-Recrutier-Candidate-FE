b64451e044c2d1f5e2add7746990d452dd3fe931
NavyaGunda23 <52776081+NavyaGunda23@users.noreply.github.com>
2025-09-04 16:37:02 +0400

    updated cloud url

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://yfwgrlomilroyvzmqwql.supabase.co';
+const supabaseUrl = 'https://supabaserecruiter.innovasense.net';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlmd2dybG9taWxyb3l2em1xd3FsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3Njk1NzAsImV4cCI6MjA2NjM0NTU3MH0.fSn6H0EEJgdgwZIZc3xovyR8l-yclhoxAW7J8bQeboI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
