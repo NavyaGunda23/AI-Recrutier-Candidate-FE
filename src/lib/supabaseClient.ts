6f12fb2d1ebe40ac166c56773835dd5c8b438e79
NavyaGunda23 <navyagunda.71@gmail.com>
2025-09-24 09:56:21 +0400

    reverted to existing supabse url

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,6 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://yfwgrlomilroyvzmqwql.supabase.co';
+
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlmd2dybG9taWxyb3l2em1xd3FsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3Njk1NzAsImV4cCI6MjA2NjM0NTU3MH0.fSn6H0EEJgdgwZIZc3xovyR8l-yclhoxAW7J8bQeboI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
